89a7d8414b470d1aab66ba5b089a66ed09d141a5
Suzanne Richman <suzanne.richman@avanade.com>
2020-03-17 09:34:18 -0700

    removed log and fixed formatting in files

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -6,40 +6,38 @@ type Props = {
 	data: Data;
 };
 
-function renderEvolution(evolutions: Evolution[]){
-  if(!evolutions || evolutions.length === 0) return<p></p>
-  else 
-  return(
-    <>
-    <h4><em>Evolutions:</em></h4>
-    {evolutions.map((evo, indx) => (
-      <p key={indx}>{evo.name}</p>
-    ))}
-    </>
-  )
+function renderEvolution(evolutions: Evolution[]) {
+	if (!evolutions || evolutions.length === 0) return <p></p>;
+	else
+		return (
+			<>
+				<h4>
+					<em>Evolutions:</em>
+				</h4>
+				{evolutions.map((evo, indx) => (
+					<p key={indx}>{evo.name}</p>
+				))}
+			</>
+		);
 }
-function renderPokemon(pokemons:Pokemon[]){
-if(!pokemons || pokemons.length === 0) return <div>No Pokemon Found :(</div>
-return(
-  <>
-  {pokemons.map((pokemon,index) => (
-    <div key={index} className="card">
-      <img src={pokemon.image}/>
-      <div className="card-body">
-        <h3>{pokemon.name}</h3>
-        {renderEvolution(pokemon.evolutions)}
-      </div>
-    </div>
-  ))}
-  </>
-)
-
+function renderPokemon(pokemons: Pokemon[]) {
+	if (!pokemons || pokemons.length === 0)
+		return <div>No Pokemon Found :(</div>;
+	return (
+		<>
+			{pokemons.map((pokemon, index) => (
+				<div key={index} className="card">
+					<img src={pokemon.image} />
+					<div className="card-body">
+						<h3>{pokemon.name}</h3>
+						{renderEvolution(pokemon.evolutions)}
+					</div>
+				</div>
+			))}
+		</>
+	);
 }
 
 export default function Character({ data: { pokemons } }: Props) {
-	return (
-		<div className="container">
-      {renderPokemon(pokemons)}
-		</div>
-	);
+	return <div className="container">{renderPokemon(pokemons)}</div>;
 }
--- src/hooks/useInfo.ts
+++ src/hooks/useInfo.ts
@@ -1,40 +1,40 @@
-import {useQuery} from '@apollo/react-hooks';
+import { useQuery } from "@apollo/react-hooks";
 import { QueryResult } from "@apollo/react-common";
 import gql from "graphql-tag";
 
-const GET_POKEMON_INFO = gql `
-{
-    pokemons(first: 150) {
-      id
-      number
-      name,
-      image,
-      evolutions {
-        id,
-        number,
-        name,
-        image
-      }
-    }
-  }`
+const GET_POKEMON_INFO = gql`
+	{
+		pokemons(first: 832) {
+			id
+			number
+			name
+			image
+			evolutions {
+				id
+				number
+				name
+				image
+			}
+		}
+	}
+`;
 
-  
 export type Data = {
-  pokemons: Pokemon[];
-}
+	pokemons: Pokemon[];
+};
 
-export type Pokemon ={
-  id: string;
-  image: string;
-  name: string;
-  evolutions: Evolution[];
-}
+export type Pokemon = {
+	id: string;
+	image: string;
+	name: string;
+	evolutions: Evolution[];
+};
 
 export type Evolution = {
-  name: string;
-}
+	name: string;
+};
 
-export function useInfo():QueryResult {
-  const result =  useQuery<QueryResult>(GET_POKEMON_INFO)
-  return result;
+export function useInfo(): QueryResult {
+	const result = useQuery<QueryResult>(GET_POKEMON_INFO);
+	return result;
 }
